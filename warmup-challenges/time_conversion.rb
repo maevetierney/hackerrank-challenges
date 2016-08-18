055ac23b0b83d91baf3d061843a3fa522781c09d
Maeve Tierney <maevet27@gmail.com>
2016-08-18 17:28:56 -0500

    created warmup-challenges folder, added in first time conversion warmup challenge and added in instructions in comment form

---
+++ warmup-challenges/time_conversion.rb
@@ -0,0 +1,15 @@
+# Given a time in AM/PM format, convert it to military (-hour) time.
+# Note: Midnight is 12:00:00AM on 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a hour clock, and 12:00:00 on a -hour clock.
+
+# Input Format: A single string containing a time in AM/PM
+
+# Output Format: Military time string
+
+# Convert and print the given time in -hour format, where .
+
+# Sample Input
+    # 07:05:45PM
+# Sample Output
+    # 19:05:45
+
+--------------------
