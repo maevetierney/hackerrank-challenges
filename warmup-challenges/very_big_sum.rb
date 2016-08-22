3851b972b4cea5627120141795bd4b8c644c079d
Maeve Tierney <maevet27@gmail.com>
2016-08-21 19:45:43 -0500

    Completed solution

--- warmup-challenges/very_big_sum.rb
+++ warmup-challenges/very_big_sum.rb
@@ -7,27 +7,50 @@
     #add the last # on each block to the LAST variable
 
 
-######CODE############################
+# ######CODE############################
 
-n = 5
-arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
-num_of_blocks = arr.length
-last = []
-new_arr = [0,0,0,0,0,0,0,0,0,0,]
-i = 0
-while i <= n
-    arr.map! do |chunk|
-        chunk.to_s.chars
-        chunk.to_s.chars.each do |num|
-            last << chunk.to_s.chars.last
-            i += 1
-        end
-    end
-end
-new_arr.unshift(n)
-new_arr.push(last)
-new_arr.join
+# n = 5
+# arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+# num_of_blocks = arr.length
+# last = []
+# new_arr = [0,0,0,0,0,0,0,0,0,0,]
+# i = 0
+
+# while i <= n
+#     arr.map! do |chunk|
+#         chunk.to_s.chars
+#         chunk.to_s.chars.each do |num|
+#             last << chunk.to_s.chars.last
+#         end
+#       i += 1
+#     end
+# end
+# new_arr.unshift(n)
+# new_arr.push(last)
+# puts new_arr.join
 
 
 #PLACE first with N
 #PLACE last with last
+
+
+
+
+arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+n = arr.length
+num_of_blocks = arr.length
+last = []
+new_num = [0,0,0,0,0,0,0,0,0,0]
+#turn each chunk into an array
+#each num to a single string
+last = 0
+total = arr.length
+arr.each do |num|
+  num_array = num.to_s.chars
+  # print num_array
+  last += num_array.last.to_i
+end
+new_num[0] = n
+new_num[-1] = last
+print new_num.join
