77a21bd6922c8258ee51fd4f7e10b5c0394d263d
Maeve Tierney <maevet27@gmail.com>
2016-08-21 22:40:35 -0500

    Completed solution

--- interview-cake-problems/get_product_of_list_expt_indx.rb
+++ interview-cake-problems/get_product_of_list_expt_indx.rb
@@ -11,19 +11,51 @@
 
 ## CODE ###############
 
-def get_total_product_of_each_indx_in_array_excpet_itself(array)
-    current_index = 0
-    look at array[0]
-    answer = 0
+# def get_total_product_of_each_indx_in_array_excpet_itself(array)
+#     current_index = 1
+#     answer = 0
 
-    until array[-1] == array[current_index]
-        array.select do |index|
-            if index != current_index
-                answer *= array[index]
-            end
-        end
-        current_index += 1
-    end
+#     until array[-1] == array[current_index]
+#         array.select do |num|
+#           puts num
+#           if num =! array[current_index]
+#             puts current_index
+#             answer *= num
+#           end
+#         end
+#         current_index += 1
+#     end
+#   puts answer
+# end
+# test_array = [1,7,3,4]
+# get_total_product_of_each_indx_in_array_excpet_itself(test_array)
+
+
+# def get_total_product_of_each_indx_in_array_excpet_itself(array)
+#     current_index = 0
+#     answer = 0
+#     max_index = array.length
+
+#     p array.reduce(1, :*)
+#     #(5..10).inject(1) { |product, n| product * n }
+# end
+
+#  (5..10).reduce(1, :*)
+
+
+# test_array = [1,7,3,4]
+# get_total_product_of_each_indx_in_array_excpet_itself(test_array)
+
+def get_total_product_of_each_indx_in_array_excpet_itself(array)
+  answer_array = []
+  array.each_index do |index|
+    new_array = array.select { |num| num != array[index] }
+    indx_sum = new_array.reduce(1, :*)
+    answer_array << indx_sum
+  end
+  puts answer_array
 end
 
-puts get_total_product_of_each_indx_in_array_excpet_itself([1,7,3,4])
+
+test_array = [1,7,3,4]
+get_total_product_of_each_indx_in_array_excpet_itself(test_array)
