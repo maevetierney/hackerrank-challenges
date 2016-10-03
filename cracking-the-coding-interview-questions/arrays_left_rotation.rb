3c21bea8eabb02dc8e28140762c8ae33878f7468
Maeve Tierney <maevet27@gmail.com>
2016-10-02 20:38:30 -0400

    Created psuedocode, and first draft of method, going to test it now and improve

--- cracking-the-coding-interview-questions/arrays_left_rotation.rb
+++ cracking-the-coding-interview-questions/arrays_left_rotation.rb
@@ -12,13 +12,38 @@
 
   # Then print the updated array as a single line of space-separated integers.
 
+  #Sample Input
+    # 5 4
+    # 1 2 3 4 5
+
+  #Sample Output
+    #5 1 2 3 4
+
 ########################
 
 #Psuedocode:
-  #A left rotation operation on an array of size shifts each of the arrays elements  unit to the left.
+  #A left rotation operation on an array of size shifts each of the arrays elements unit to the left.
 
   #For example, if 2 left rotations are performed on array [1,2,3,4,5] , then the array would become [3,4,5,1,2].
 
   # Given an array of n integers and a number, d, perform d left rotations on the array.
 
   # Then print the updated array as a single line of space-separated integers.
+
+########################
+
+#First Draft Experimentation
+array_of_n_integers = [1,2,3,4,5]
+n = array_of_n_integers
+number_of_rotations = 0
+d = number_of_rotations
+
+def left_rotations(number_of_rotations)
+  #D will become the number of 0's we are going to inject into the beginning of this array.
+  number_of_rotations.times do
+    array_of_n_integers.unshift(0)
+  end
+  #drop those 0's you added in (hint: its the first D number of indicies)
+  array_of_n_integers.reject {|num| num < 1}
+  #this also returns the new array 
+end
