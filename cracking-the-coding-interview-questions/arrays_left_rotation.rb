ee70ea9c808f28d9b4ed97954cad19125a101d07
Maeve Tierney <maevet27@gmail.com>
2016-10-02 20:02:48 -0400

    Setting up challenge and adding in psuedocode, reading the instructions and setting up test

---
+++ cracking-the-coding-interview-questions/arrays_left_rotation.rb
@@ -0,0 +1,24 @@
+# Problem Title: Arrays: Left Rotation
+
+  #note: found on hackerrank.com but is originally a problem featured in Cracking the Coding Interview.
+#########################
+
+# Objective :
+  #A left rotation operation on an array of size shifts each of the arrays elements  unit to the left.
+
+  #For example, if 2 left rotations are performed on array [1,2,3,4,5] , then the array would become [3,4,5,1,2].
+
+  # Given an array of n integers and a number, d, perform d left rotations on the array.
+
+  # Then print the updated array as a single line of space-separated integers.
+
+########################
+
+#Psuedocode: 
+  #A left rotation operation on an array of size shifts each of the arrays elements  unit to the left.
+
+  #For example, if 2 left rotations are performed on array [1,2,3,4,5] , then the array would become [3,4,5,1,2].
+
+  # Given an array of n integers and a number, d, perform d left rotations on the array.
+
+  # Then print the updated array as a single line of space-separated integers.
